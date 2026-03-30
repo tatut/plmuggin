cef0671be88e51a9368c6fac09828c38fa47cc4a
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2026-03-30 17:20:06 +0300

    Add endpoint with path parameters and test

--- mugshot/test/test.sql
+++ mugshot/test/test.sql
@@ -16,3 +16,10 @@ html
   body(m:q="SELECT {{a}} + {{b}} as c")
     div.add {{a}} + {{b}} = {{c}}
 $$ LANGUAGE plmuggin;
+
+CREATE FUNCTION web."GET/:year/:month/:date"(year INTEGER, month INTEGER, "date" INTEGER)
+RETURNS "text/html" AS $$
+html
+  body(m:q="SELECT (make_date({{year}},{{month}},{{date}}) - '1970-01-01'::DATE) as days")
+    div Date {{date}}.{{month}}.{{year}} is {{days}} days after the UNIX epoch
+$$ LANGUAGE plmuggin;
