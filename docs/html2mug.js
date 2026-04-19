8376ae938ec6374a3ef0966ef47629b089877083
Tatu Tarvainen <tatu.tarvainen@me.com>
2026-04-19 12:59:25 +0300

    Add HTML->Muggin conversion

---
+++ docs/html2mug.js
@@ -0,0 +1,50 @@
+function ind(indent) {
+    if(indent === 0) return "";
+    else return ("  " + ind(indent-1));
+}
+
+function mug(indent, node) {
+    var out = ind(indent) + node.tagName.toLowerCase();
+    var attrs = node.attributes.length;
+    if(node.hasAttribute("id")) {
+        out += "#" + node.getAttribute("id");
+        attrs -= 1;
+    }
+    if(node.hasAttribute("class")) {
+        var classes = node.getAttribute("class").split(/ +/);
+        for(var c=0;c<classes.length;c++) {
+            out += "." + classes[c];
+        }
+        attrs -= 1;
+    }
+    if(attrs > 0) {
+        out += "(";
+        var first = true;
+        for(var i=0;i<node.attributes.length;i++) {
+            let a = node.attributes[i];
+            if(a.name == "class" || a.name == "id") continue;
+            if(!first) out += " ";
+            out += a.name + "=\"" + a.value + "\"";
+            first = false;
+        }
+        out += ")";
+    }
+    out += "\n";
+    indent += 2;
+    node.childNodes.forEach((c) => {
+        out += mug(indent, c);
+    });
+    return out;
+}
+
+function html2mug() {
+    const node = document.createNode("html");
+    node.innerHTML = document.querySelector("#src").value;
+    const dst = document.querySelector("#dst");
+    dst.value = "";
+    var n = node.firstElementChild;
+    while(n != null) {
+        dst.value += mug(0, n) + "\n";
+        n = n.nextElementSibling;
+    }
+}
---
+++ docs/html2mug.md
@@ -0,0 +1,6 @@
+# HTML to Muggin template
+
+ <script src="html2mug.js">
+ <textarea id="src" placeholder="Paste HTML here" onchange="html2mug"></textarea>
+
+ <textarea id="dst" placeholder="Converted template here"></textarea>
